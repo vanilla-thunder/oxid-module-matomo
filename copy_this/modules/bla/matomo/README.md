77a099800e4ec06009df886032f8a6c9436db410
vanilla-thunder <1874024+vanilla-thunder@users.noreply.github.com>
2018-11-30 10:01:44 +0100

    v2.4

--- copy_this/modules/bla/matomo/README.md
+++ copy_this/modules/bla/matomo/README.md
@@ -1,10 +1,67 @@
-## Matomo (Piwik) Analytics for OXID eShop
-by  bestlife AG <oxid@bestlife.ag>  
-module version 2.3.0 2018-2-16  
-https://github.com/vanilla-thunder/oxid-module-bla-matomo
+## [bla] matomo - Matomo (Piwik) Analytics for OXID eShop
+version 2.4.0 ( 2018-11-30 )
+
+## Funktionen:
+1. Tracking der Seitenaufrufe
+2. Shopinterne Suche
+3. E-Commerce Tracking
+4. benutzerdefinierte Dimensionen
+5. Geo Location ( Besucher-IP nach Land auflösen ) 
+
+##  Installation:
+1. [https://github.com/vanilla-thunder/oxid-module-matomo/archive/master.zip](https://github.com/vanilla-thunder/oxid-module-matomo/archive/master.zip) herunterladen und entpacken
+2. Inhalt von "copy_this" in den Shop hochladen
+3. Modul aktivieren und Moduleinstellungen konfigurieren
+4. Views aktualisieren
+5. TPL Blocks hinzufügen: in **email/html/header.tpl** direkt nach ``<body ... >``  einfügen:  
+    ````
+    [{block name="email_html_header_tracking"}][{/block}]
+    ````
+
+## Konfiguration:
+unter Erweiterungen -> Module -> [bestlife] Matomo Tracking -> Reiter "Einstellungen":
+- **Matomo Server URL** und **Site ID** werden benötigt, damit überhaupt irgendwas funktioniert.
+- **Auth Token** wird nur für Tracking über API *(PHP/Serverseitig, wird aktuell nicht verwendet)* und GeoIP Funktion benötigt
+- Sofern Ziele und benutzerdefinierte Dimensionen gebraucht werden, müssen diese in Matomo erstellt und deren IDs in den Moduleinstellungen hinterlegt werden
+
+### Konfiguration der Ziele in matomo
+1. Newsletter Opt-In
+    - Ziel ist erreicht: manuell
+2. Newsletter Freischaltung (Opt-In bestätigt)
+    - Ziel ist erreicht: manuell  
+3. Newsletter Abmeldung
+    - Ziel ist erreicht: manuell
+4. Neue Registrierung
+    - "Wenn vom Besucher" + "Die angegebene URL wird besucht" 
+    - die URL beinhaltet ``/index.php?cl=register&success=1``
+5. Neue Registrierung im Checkout
+    - "Wenn vom Besucher" + "Die angegebene URL wird besucht" 
+    - die URL beinhaltet ``/index.php?cl=payment&new_user=1&success=1``
+6. Neue Bestellung
+    - "Wenn vom Besucher" + "Die angegebene URL wird besucht" 
+    - die URL beinhaltet ``/index.php?cl=thankyou``
+  
+### Benutzerdefinierte Dimensionen  
+Das Modul unterstützt standardmäßig 5 benutzerdefinierte Dimensionen:
+- Sprache
+- Zahlungsart
+- Versandart
+- Versandadresse ( Rechnungsadresse / Packstation / Abweichende Lieferanschrift )
+- Mediacode / REF
+
+Um die benutzerdefinierten Dimensionen zu verwenden, muss das "custom dimensions" Plugin installiert und anschließend die entsprechenden Dimensionen erstellt werden.   
+Mehr Infos zu benutzerdefinierten Dimensionen gibts hier: [https://matomo.org/docs/custom-dimensions/](https://matomo.org/docs/custom-dimensions/)
+
+### Geo Location
+Sofern Geo Location in Matomo konfiguriert ist, kann das Herkunftsland über die IP Adresse der Besucher bestimmt werden.  
+Die Funktion ``[{$oViewConf->getCountryByIP()}]``  liefert den ISO Alpha-2 Kürzel ("DE", "AT", "US" etc) des lande.   
+Weitere Infos zu Geo Location in Matomo: https://matomo.org/docs/geo-locate/
+
+Beipsiel Code: ``[{if $oViewConf->getCountryByIP() == "DE"}]Kunde aus DE[{/if}]`` 
+
 
 ### LICENSE AGREEMENT
-   Matomo (Piwik) Analytics for OXID eShop  
+   [bla] matomo - Matomo (Piwik) Analytics for OXID eShop  
    Copyright (C) 2018 bestlife AG  
    info:  oxid@bestlife.ag  
   
--- copy_this/modules/bla/matomo/application/views/admin/de/matomo_lang.php
+++ copy_this/modules/bla/matomo/application/views/admin/de/matomo_lang.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Matomo (Piwik) Analytics for OXID eShop
+ * [bla] matomo
  * Copyright (C) 2018  bestlife AG
  * info:  oxid@bestlife.ag
  *
@@ -34,41 +34,71 @@ $css = '<style type="text/css">
 .groupExp dd > h3 { display: inline-block; margin: 0 15px; border: none; }
 .groupExp a.rc b {font-size:medium;color:#ff3600;}.groupExp dt input.txt {width:430px !important} .groupExp dl {display:block !important;} input.confinput {position:fixed;top:20px;right:70px;background:#008B2D;padding:10px 25px;color:white;border:1px solid black;cursor:pointer;font-size:125%;} input.confinput:hover {outline:3px solid #ff3600;} .groupExp dt textarea.txtfield {width:430px;height:150px;}</style>';
 $aLang = [
-    'charset'                                     => 'UTF-8',
-    'SHOP_MODULE_GROUP_blaMatomo_Main'             => $css . 'Matomo Konfiguration',
-    'SHOP_MODULE_blaMatomo_debug'                  => 'Debug Modus (zeigt für Shop-Admins Matomo Tracking Variablen im Template an)',
-    'SHOP_MODULE_blaMatomo_sUrl'                   => '<h3>Matomo Server URL</h3><em>Hier die URL zu Ihrem Matomo Server eintragen.</em><br><b>Format:</b><i> ohne http:// bzw. https:// und ohne abschliessendem /</i><br><b>Bsp:</b> <i>matomo.domain.tld</i>',
-    'SHOP_MODULE_blaMatomo_sToken'                 => '<h3>Auth Token</h3><em>Kann unter <b>Administration -> Plattform -> API</b> vom entsprechenden Benutzer kopiert werden</em>',
-    'SHOP_MODULE_blaMatomo_iPageid'                => '<h3>Matomo Seiten-ID</h3><em>Tragen Sie hier die Matomo Seiten-ID ein.</em>',
-    'SHOP_MODULE_blaMatomo_iNewsgoalid'            => '<h3>Newsletter Ziel-ID</h3><em>Wenn Sie in Matomo ein Ziel für eine Newsletterbestellung definiert haben, tragen Sie hier die Ziel-ID ein. Sollte dies nicht benötigt werden tragen Sie eine 0 ein.</em>',
-    'SHOP_MODULE_blaMatomo_iMaxCustVar'            => '<h3>Anzahl benutzerdefinierter Variablen</h3><em>Matomo erlaubt standardmässig nur 5 benutzerdefinierte Variablen pro Scope. Dies kann in der Matomo Konfiguration erweitert werden. Ändern sie diesen Wert nur wenn sie sich sicher sind und diese Anleitung ausgeführt haben <a href="http://matomo.org/faq/how-to/faq_17931/" target="_blank">http://matomo.org/faq/how-to/faq_17931/</a>.</em>',
-    'SHOP_MODULE_blaMatomo_iCustindexNewsletter'   => '<h3>Index der benutzerdefinierte Variable für das Newsletter Anmeldungstracking</h3><em>Matomo page-scope Index der <a href="http://matomo.org/docs/custom-variables/" target="_blank" title="Was ist eine benutzerdefinierte Variable">benutzerdefinierten Variable</a> zwischen 1 und 5 wo das Newsletter Anmeldungstracking gespeichert werden soll. (0 zum Deaktivieren)</em>',
-    'SHOP_MODULE_blaMatomo_iCustindexPayment'      => '<h3>Index der benutzerdefinierten Variable für Zahlungsart-Tracking</h3><em>Matomo visit-scope Index der <a href="http://matomo.org/docs/custom-variables/" target="_blank" title="Was ist eine benutzerdefinierte Variable">benutzerdefinierten Variable</a> zwischen 1 und 5 wo die Zahlungsart gespeichert werden soll. (0 zum Deaktivieren)</em>',
-    'SHOP_MODULE_blaMatomo_blUseUserID'            => '<h3>Eindeutige User ID zum Tracken der Kunden verwenden</h3><em>Anhaken um das erweiterte User ID Tracking zwischen Geräten und Browsern von Matomo zu verwenden. Weitere Informationen was für Auswirkungen dies hat finden sie in der Matomo Dokumentation zum Thema <a href="http://matomo.org/docs/user-id/" target="_blank">User ID</a>. Dieses Feature erforder Matomo >= 2.7.0</em>',
-    'SHOP_MODULE_blaMatomo_blFirstReferrerConv'    => '<h3>Ersten Referrer einer Conversion zuordnen</h3><em>Anhaken um den ersten Referrer einer Conversion zuzuordnen. Standardmässig wird der zuletzte verwendete Referrer einer Conversion zugeordnet.</em>',
-    'SHOP_MODULE_blaMatomo_blEnableJSErrorTrackin' => '<h3>Unbehandelte Frontend JavaScript-Fehler tracken</h3><em>Diese Option trackt unbehandelte JavaScript-Fehler im Shop-Frontend in Matomo\'s <a href="http://matomo.org/docs/event-tracking/" target="_blank" title="Event Tracking Dokumentation">Event Tracking</a>.<br>Achtung, dieses Feature erfordert Matomo >= 2.2.0</em>',
-// Custom variable labels
-    'SHOP_MODULE_GROUP_blaMatomo_CustomVars'       => 'Bezeichnungen für benutzerdefinierte Variablen',
-    'SHOP_MODULE_blaMatomo_CUSTOM_NEWSLETTER'      => 'Newsletter',
-    'SHOP_MODULE_blaMatomo_sCustomvarNewsletter'   => 'Text der in Matomo bei der benutzerdefinierten Varible für das Newsletter Anmeldungstracking erscheint.',
-    'SHOP_MODULE_blaMatomo_CUSTOM_PAYMENT'         => 'Zahlungsart',
-    'SHOP_MODULE_blaMatomo_sCustomvarPayment'      => 'Text der in Matomo bei der benutzerdefinierten Varible für das Zahlungsarttracking erscheint.',
-    'SHOP_MODULE_blaMatomo_NEWSLETTER_ACCOUNT'     => 'Konto Newslettereinstellungen gesehen',
-    'SHOP_MODULE_blaMatomo_sNewsletterAccount'     => 'Text der in Matomo angezeigt wird wenn ein Besucher in den Kontoeinstellungen auf der Newslettereinstellungsseite war.',
-    'SHOP_MODULE_blaMatomo_NEWSLETTER_ACCOUNT_ON'  => 'Konto Newsletter aktiviert',
-    'SHOP_MODULE_blaMatomo_sNewsletterAccountOn'   => 'Text der in Matomo angezeigt wird wenn ein Besucher in den Kontoeinstellungen den Newsletter aktiviert hat.',
-    'SHOP_MODULE_blaMatomo_NEWSLETTER_ACCOUNT_OFF' => 'Konto Newsletter deaktiviert',
-    'SHOP_MODULE_blaMatomo_sNewsletterAccountOff'  => 'Text der in Matomo angezeigt wird wenn ein Besucher in den Kontoeinstellungen den Newsletter deaktiviert hat.',
-    'SHOP_MODULE_blaMatomo_NEWSLETTER_VIEWED'      => 'Angeschaut',
-    'SHOP_MODULE_blaMatomo_sNewsletterViewed'      => 'Text der in Matomo angezeigt wird wenn ein Besucher auf der Newsletter Anmeldeseite war.',
-    'SHOP_MODULE_blaMatomo_NEWSLETTER_ORDERED'     => 'Angemeldet',
-    'SHOP_MODULE_blaMatomo_sNewsletterOrdered'     => 'Text der in Matomo angezeigt wird wenn ein Besucher das Newsletter Anmeldeformular abgeschickt hat.',
-    'SHOP_MODULE_blaMatomo_NEWSLETTER_ACTIVATED'   => 'Best&auml;tigt',
-    'SHOP_MODULE_blaMatomo_sNewsletterActivated'   => 'Text der in Matomo angezeigt wird wenn ein Besucher den Newsletter Double-Opt-In bestätigt hat.',
-    'SHOP_MODULE_blaMatomo_NEWSLETTER_CANCELED'    => 'Gel&ouml;scht',
-    'SHOP_MODULE_blaMatomo_sNewsletterCanceled'    => 'Text der in Matomo angezeigt wird wenn ein Besucher den Newsletter abbestellt hat.',
-// additional mapping
-    'SHOP_MODULE_GROUP_blaMatomo_Params'           => 'Zusätzliches Parametermapping',
-    'SHOP_MODULE_blaMatomo_aParamMapVisit'         => '<h3>Parameter Mapping für visit-scope benutzerdefinierte Variablen</h3><em>Hierüber können pro Zeile ein URL Parameter in Matomos visit-scope <a href="http://matomo.org/docs/custom-variables/" target="_blank" title="Was ist eine benutzerdefinierte Variable">benutzerdefinierten Variablen</a> gespeichert werden. (leer lassen wenn nicht benötigt)<p><b>Konfiguration:</b> parameter => index|Bezeichnung<br><code>foo => 4|Parameter 1<br>foo2 => 5|Parameter2</code><br>Bei einem Aufruf von "http://www.shop.tld/index.php?foo=bar" wird in Matomo in die benutzerdefinierte Variable mit dem Index 4 und dem Namen "Parameter 1" der Wert "bar" gespeichert.<br><b>Achtung</b> ein visit-scope Index wird auch schon für das Zahlungsart-Tracking verwendet!</p></em>',
-    'SHOP_MODULE_blaMatomo_aParamMapPage'          => '<h3>Parameter Mapping für page-scope benutzerdefinierte Variablen</h3><em>Hierüber können pro Zeile ein URL Parameter in Matomos page-scope <a href="http://matomo.org/docs/custom-variables/" target="_blank" title="Was ist eine benutzerdefinierte Variable">benutzerdefinierten Variablen</a> gespeichert werden. (leer lassen wenn nicht benötigt)<br> Funktionsweise wie beim visit-scope Parameter-Mapping.<br><b>Achtung</b> ein page-scope Index wird auch schon für das Newsletter-Tracking verwendet!</em>'
+	'charset'                                         => 'UTF-8',
+	'SHOP_MODULE_GROUP_blaMatomo_Main'                => $css . 'Matomo Konfiguration',
+	'SHOP_MODULE_blaMatomo_sUrl'                      => '<h3>Matomo Server URL</h3><em>Beispiel: https://matomo.domain.tld/</em>',
+	'SHOP_MODULE_blaMatomo_sToken'                    => '<h3>Auth Token</h3><em>Kann unter <b>Administration -> Plattform -> API</b> vom entsprechenden Benutzer kopiert werden</em>',
+	'SHOP_MODULE_blaMatomo_iSiteID'                   => '<h3>Matomo Seiten-ID</h3><em>Tragen Sie hier die Matomo Seiten-ID ein.</em>',
+	'SHOP_MODULE_blaMatomo_blUseUserID'               => '<h3>Eindeutige User ID zum Tracken der Kunden verwenden</h3><em>Anhaken um das erweiterte User ID Tracking zwischen Geräten und Browsern von Matomo zu verwenden. Weitere Informationen was für Auswirkungen dies hat finden sie in der Matomo Dokumentation zum Thema <a href="http://matomo.org/docs/user-id/" target="_blank">User ID</a>. Dieses Feature erforder Matomo >= 2.7.0</em>',
+	'SHOP_MODULE_blaMatomo_blEnableJSErrorTrackin'    => '<h3>Unbehandelte Frontend JavaScript-Fehler tracken</h3><em>Diese Option trackt unbehandelte JavaScript-Fehler im Shop-Frontend in Matomo\'s <a href="http://matomo.org/docs/event-tracking/" target="_blank" title="Event Tracking Dokumentation">Event Tracking</a>.<br>Achtung, dieses Feature erfordert Matomo >= 2.2.0</em>',
+	'SHOP_MODULE_blaMatomo_blFirstReferrerConv'       => '<h3>Ersten Referrer einer Conversion zuordnen</h3><em>Anhaken um den ersten Referrer einer Conversion zuzuordnen. Standardmässig wird der zuletzte verwendete Referrer einer Conversion zugeordnet.</em>',
+
+	// goals
+	'SHOP_MODULE_GROUP_blaMatomo_Goals'                => 'Ziele',
+	'SHOP_MODULE_blaMatomo_iNewsletterOptinGoal'       => 'Ziel-ID für Newsletter-Opt-In Bestätigung',
+	'SHOP_MODULE_blaMatomo_iNewsletterSubscribedGoal'  => 'Ziel-ID für Newsletter-Anmeldungen',
+	'SHOP_MODULE_blaMatomo_iNewsletterUnsubscribedGoal'=> 'Ziel-ID für Newsletter-Abmeldungen',
+
+	// custom dimensions
+	'SHOP_MODULE_GROUP_blaMatomo_CustomDimensions'    => 'Benutzerdefinierte Dimensionen',
+	'SHOP_MODULE_blaMatomo_iLanguageDimension'        => 'Index der Sprach-Dimension',
+	'SHOP_MODULE_blaMatomo_iPaymentDimension'         => 'Index der Zahlungsart-Dimension',
+	'SHOP_MODULE_blaMatomo_iDeliveryMetodDimension'   => 'Index der Versandart-Dimension',
+	'SHOP_MODULE_blaMatomo_iDeliveryAddressDimension' => 'Index der Lieferadressen-Dimension',
+	'SHOP_MODULE_blaMatomo_iMediacodeDimension'       => 'Index der Mediacode-Dimension',
+
+
+	// -----------------------------------------
+	'SHOP_MODULE_blaMatomo_iCustindexNewsletter'      => '<h3>Index der benutzerdefinierte Variable für das Newsletter Anmeldungstracking</h3><em>Matomo page-scope Index der <a href="http://matomo.org/docs/custom-variables/" target="_blank" title="Was ist eine benutzerdefinierte Variable">benutzerdefinierten Variable</a> zwischen 1 und 5 wo das Newsletter Anmeldungstracking gespeichert werden soll. (0 zum Deaktivieren)</em>',
+	'SHOP_MODULE_blaMatomo_iCustindexPayment'         => '<h3>Index der benutzerdefinierten Variable für Zahlungsart-Tracking</h3><em>Matomo visit-scope Index der <a href="http://matomo.org/docs/custom-variables/" target="_blank" title="Was ist eine benutzerdefinierte Variable">benutzerdefinierten Variable</a> zwischen 1 und 5 wo die Zahlungsart gespeichert werden soll. (0 zum Deaktivieren)</em>',
+
+
+	'SHOP_MODULE_blaMatomo_CUSTOM_NEWSLETTER'      => 'Newsletter',
+	'SHOP_MODULE_blaMatomo_sCustomvarNewsletter'   => 'Text der in Matomo bei der benutzerdefinierten Varible für das Newsletter Anmeldungstracking erscheint.',
+	'SHOP_MODULE_blaMatomo_CUSTOM_PAYMENT'         => 'Zahlungsart',
+	'SHOP_MODULE_blaMatomo_sCustomvarPayment'      => 'Text der in Matomo bei der benutzerdefinierten Varible für das Zahlungsarttracking erscheint.',
+	'SHOP_MODULE_blaMatomo_NEWSLETTER_ACCOUNT'     => 'Konto Newslettereinstellungen gesehen',
+	'SHOP_MODULE_blaMatomo_sNewsletterAccount'     => 'Text der in Matomo angezeigt wird wenn ein Besucher in den Kontoeinstellungen auf der Newslettereinstellungsseite war.',
+	'SHOP_MODULE_blaMatomo_NEWSLETTER_ACCOUNT_ON'  => 'Konto Newsletter aktiviert',
+	'SHOP_MODULE_blaMatomo_sNewsletterAccountOn'   => 'Text der in Matomo angezeigt wird wenn ein Besucher in den Kontoeinstellungen den Newsletter aktiviert hat.',
+	'SHOP_MODULE_blaMatomo_NEWSLETTER_ACCOUNT_OFF' => 'Konto Newsletter deaktiviert',
+	'SHOP_MODULE_blaMatomo_sNewsletterAccountOff'  => 'Text der in Matomo angezeigt wird wenn ein Besucher in den Kontoeinstellungen den Newsletter deaktiviert hat.',
+	'SHOP_MODULE_blaMatomo_NEWSLETTER_VIEWED'      => 'Angeschaut',
+	'SHOP_MODULE_blaMatomo_sNewsletterViewed'      => 'Text der in Matomo angezeigt wird wenn ein Besucher auf der Newsletter Anmeldeseite war.',
+	'SHOP_MODULE_blaMatomo_NEWSLETTER_ORDERED'     => 'Angemeldet',
+	'SHOP_MODULE_blaMatomo_sNewsletterOrdered'     => 'Text der in Matomo angezeigt wird wenn ein Besucher das Newsletter Anmeldeformular abgeschickt hat.',
+	'SHOP_MODULE_blaMatomo_NEWSLETTER_ACTIVATED'   => 'Best&auml;tigt',
+	'SHOP_MODULE_blaMatomo_sNewsletterActivated'   => 'Text der in Matomo angezeigt wird wenn ein Besucher den Newsletter Double-Opt-In bestätigt hat.',
+	'SHOP_MODULE_blaMatomo_NEWSLETTER_CANCELED'    => 'Gel&ouml;scht',
+	'SHOP_MODULE_blaMatomo_sNewsletterCanceled'    => 'Text der in Matomo angezeigt wird wenn ein Besucher den Newsletter abbestellt hat.',
+	// additional mapping
+	'SHOP_MODULE_GROUP_blaMatomo_Params'           => 'Zusätzliches Parametermapping',
+	'SHOP_MODULE_blaMatomo_aParamMapVisit'         => '<h3>Parameter Mapping für visit-scope benutzerdefinierte Variablen</h3><em>Hierüber können pro Zeile ein URL Parameter in Matomos visit-scope <a href="http://matomo.org/docs/custom-variables/" target="_blank" title="Was ist eine benutzerdefinierte Variable">benutzerdefinierten Variablen</a> gespeichert werden. (leer lassen wenn nicht benötigt)<p><b>Konfiguration:</b> parameter => index|Bezeichnung<br><code>foo => 4|Parameter 1<br>foo2 => 5|Parameter2</code><br>Bei einem Aufruf von "http://www.shop.tld/index.php?foo=bar" wird in Matomo in die benutzerdefinierte Variable mit dem Index 4 und dem Namen "Parameter 1" der Wert "bar" gespeichert.<br><b>Achtung</b> ein visit-scope Index wird auch schon für das Zahlungsart-Tracking verwendet!</p></em>',
+	'SHOP_MODULE_blaMatomo_aParamMapPage'          => '<h3>Parameter Mapping für page-scope benutzerdefinierte Variablen</h3><em>Hierüber können pro Zeile ein URL Parameter in Matomos page-scope <a href="http://matomo.org/docs/custom-variables/" target="_blank" title="Was ist eine benutzerdefinierte Variable">benutzerdefinierten Variablen</a> gespeichert werden. (leer lassen wenn nicht benötigt)<br> Funktionsweise wie beim visit-scope Parameter-Mapping.<br><b>Achtung</b> ein page-scope Index wird auch schon für das Newsletter-Tracking verwendet!</em>',
+
+	// content tracking
+	'SHOP_MODULE_GROUP_blaMatomo_contentTracking'  => 'Content Tracking <a href="https://matomo.org/docs/content-tracking/" target="_blank">( ? )</a>',
+	'SHOP_MODULE_blaMatomo_ct_enable'              => 'content tracking einschalten',
+	'SHOP_MODULE_blaMatomo_ct_flexprev'            => 'jQuery Selector für Slider zurück',
+	'SHOP_MODULE_blaMatomo_ct_flexnext'            => 'jQuery Selector für Slider vorwärts',
+	'SHOP_MODULE_blaMatomo_ct_zoom1'               => 'jQuery Selector für Produktbild Zoom-Ansicht Trigger',
+	'SHOP_MODULE_blaMatomo_ct_pic1'                => 'jQuery Selector für Produktbild 1 Trigger',
+	'SHOP_MODULE_blaMatomo_ct_pic2'                => 'jQuery Selector für Produktbild 2 Trigger',
+	'SHOP_MODULE_blaMatomo_ct_pic13'               => 'jQuery Selector für Produktbild 3 Trigger',
+	'SHOP_MODULE_blaMatomo_ct_pic4'                => 'jQuery Selector für Produktbild 4 Trigger',
+	'SHOP_MODULE_blaMatomo_ct_description'         => 'jQuery Selector für Produktbeschreibung (Tab) Trigger',
+	'SHOP_MODULE_blaMatomo_ct_pricealarm'          => 'jQuery Selector für Preisalarm (Tab) Trigger',
+	'SHOP_MODULE_blaMatomo_ct_tags'                => 'jQuery Selector für Produkt-Tags (Tab) Trigger',
+	'SHOP_MODULE_blaMatomo_ct_productFbComments'   => 'jQuery Selector für Facebook Kommentare (tab) Trigger',
+
 ];
